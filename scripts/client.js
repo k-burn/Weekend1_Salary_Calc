a77d200833f7c1426997e4c6cc7e80cb34710d98
k-burn <karaburnett93@gmail.com>
2018-07-29 21:55:35 -0500

    made an employee array, added ability to remove employee by number

--- scripts/client.js
+++ scripts/client.js
@@ -1,24 +1,31 @@
 console.log('JS is properly sourced in');
 
-let salaries = [];
+let employees =[];
+class Employee{
+    constructor( fName, lName, employeeNumber, jobTitle, salaryAmount ){
+      this.fname = fName;
+      this.lname = lName;
+      this.number = employeeNumber;
+      this.job = jobTitle;
+      this.salaryAmount = salaryAmount
+    }
+};
 
 $('document').ready(function(){
     clickSubmitButton()
     clickDeleteButton()
+    clickFindByNameButton()
+    clickFindByNumberButton()
 });
- 
+
  
 function clickSubmitButton() {
      $('#submitButton').on('click', function(){
-        let inputFName = $('#fNameInput').val();
-        let inputLName = $('#lNameInput').val();
-        let inputNumber = $('#employeeNumberInput').val();
-        let inputTitle = $('#jobTitleInput').val();
-        let inputSalary = $('#salaryInput').val();
          console.log('The button works!')
          collectInputData()
-         displayInputData()
-         clearInputData()
+         employeeArray()
+         displayEmployeeArray()
+         //clearInputData()
          calculateTotalSalaryCost()
 
      })  
@@ -31,10 +38,22 @@ function collectInputData(){
     let inputTitle = $('#jobTitleInput').val();
     let inputSalary = $('#salaryInput').val();
     console.log(inputFName, inputLName, inputNumber, inputSalary, inputTitle);
-    salaries.push(inputSalary);
-    console.log(salaries);
     
 };
+
+function employeeArray() {
+    let newEmployee = new Employee($('#fNameInput').val(), $('#lNameInput').val(), $('#employeeNumberInput').val(), $('#jobTitleInput').val(), $('#salaryInput').val());
+    employees.push(newEmployee);
+    console.log(employees);
+};
+
+function displayEmployeeArray(){
+    $('#tableBody').empty();
+    $('#tableBody').append('<tr><th>First Name</th><th>Last Name</th><th>ID Number</th><th>Job Title</th><th>Salary</th><th>  </th></tr>');
+    for (employee of employees){
+        $('#tableBody').append('<tr><td>'+ (employee.fname).toUpperCase()+'</td><td>'+ (employee.lname).toUpperCase()+'</td><td>'+ employee.number+'</td><td>'+ (employee.job).toUpperCase()+'</td><td> $'+ parseInt(employee.salaryAmount).toFixed(2) + '</td><td><button class="deleteButton" type="button">Delete Info</button></tr>');
+    }
+}
 
 function clearInputData(){
     $('#fNameInput').val("");
@@ -44,7 +63,8 @@ function clearInputData(){
     $('#salaryInput').val("");
 };
 
-function displayInputData() {
+
+/* function displayInputData() {
     let inputFName = $('#fNameInput').val();
     let inputLName = $('#lNameInput').val();
     let inputNumber = $('#employeeNumberInput').val();
@@ -52,12 +72,12 @@ function displayInputData() {
     let inputSalary = $('#salaryInput').val();
     console.log(inputSalary);
     $('#employeeTable').append('<tr><td>'+inputFName.toUpperCase()+'</td><td>'+inputLName.toUpperCase()+'</td><td>'+inputNumber+'</td><td>'+inputTitle.toUpperCase()+'</td><td> $'+ parseInt(inputSalary).toFixed(2) +'</td><td><button class="deleteButton" type="button">Delete Info</button></tr>');
-};
+}; */
 
 function calculateTotalSalaryCost(){
     let totalSalary = 0;
-    for (salary of salaries){
-        totalSalary = totalSalary + parseInt(salary);       
+    for (employee of employees){
+        totalSalary = totalSalary + parseInt(employee.salaryAmount);       
     }
     console.log(totalSalary);
     totalSalary = totalSalary/12;
@@ -71,8 +91,46 @@ function calculateTotalSalaryCost(){
 
 function clickDeleteButton(){
     $('#tableBody').on('click', '.deleteButton', function(){
-        console.log(this);
+        console.log('working delete button');
         $(this).parent().parent().remove(); 
     })
 };
+function clickFindByNumberButton(){
+    $('#findButton').on('click', function(){
+        console.log('button works')
+        removeEmployeeByNumber()
+    })
+}
+function clickFindByNameButton(){
+    $('#findByNameButton').on('click', function(){
+        console.log('button worksss')
+        removeEmployeeByName()
+    })
+}
  
+function removeEmployeeByNumber(){
+    let searchEmployeeNumber = $('#employeeFind').val();
+    for (let employee of employees) {
+        if (searchEmployeeNumber == employee.number){
+            console.log(employee);
+            employees.splice(employees.indexOf(employee), 1);
+            displayEmployeeArray();
+
+        }
+    }
+    calculateTotalSalaryCost()
+}
+
+function removeEmployeeByName(){
+    let searchEmployeeFName = $('#fNameFind').val();
+    let searchEmployeeLName = $('#lNameFind').val();
+    for (let employee of employees) {
+        if (searchEmployeeFName == employee.fname && searchEmployeeLName == employee.lname){
+            console.log(searchEmployeeFName);
+            employees.splice(employees.indexOf(employee), 1);
+            displayEmployeeArray();
+
+        }
+    }
+    calculateTotalSalaryCost()
+}
